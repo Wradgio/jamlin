91a5026e71b67540c1b78a9965967e50cfa23c0a
Marcel Zúbrik <wradgio@gmail.com>
2017-10-29 14:54:19 +0100

    config update, extraction update, replace update

---
+++ notes/extract translations format.js
@@ -0,0 +1,21 @@
+{
+  "translationBlocks": [
+    {
+      "name": "text name",
+      "cssSelector": "p, a",
+      "translationStrings": [
+        {
+          "stringOrig": "Menu",
+          "selector": "#mobile_menu", 
+          "translations" : [
+          	{
+							"code": "en", /* code set in program, can be in format en_US_WIN, zh_CN_#Hans, th_TH_TH_#u-nu-thai */
+							"string": "Menu EN" /* translated in translate program */
+          	}
+          ]
+        }
+      ]
+    }
+  ]
+}
+        
