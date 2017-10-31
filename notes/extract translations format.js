414b2e07cb2e342b60a85d5a33b494398ea6ba05
Marcel Zúbrik <wradgio@gmail.com>
2017-10-31 12:22:32 +0100

    extract update

--- notes/extract translations format.js
+++ notes/extract translations format.js
@@ -9,8 +9,12 @@
           "selector": "#mobile_menu", 
           "translations" : [
           	{
-							"code": "en", /* code set in program, can be in format en_US_WIN, zh_CN_#Hans, th_TH_TH_#u-nu-thai */
-							"string": "Menu EN" /* translated in translate program */
+							"langCode": "en", 
+							"translation": "Menu EN version"
+          	}, 
+          	{
+							"langCode": "sk", 
+							"translation": "Menu SK version"
           	}
           ]
         }
